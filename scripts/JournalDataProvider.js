33faffe5a6629f8e4d7ba86c70dbc832e1c9743e
Andrew Harper <druharper@gmail.com>
2020-02-28 15:51:25 -0600

    merge resolve

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,42 @@
+// const journal = [
+//   {
+//     date: "07/24/2025",
+//     concept: "HTML & CSS",
+//     entry:
+//       "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+//     mood: "Ok"
+//   }
+// ]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+// export const useJournalEntries = () => {
+//   const sortedByDate = journal.sort(
+//     (currentEntry, nextEntry) =>
+//       Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+//   )
+//   return sortedByDate
+// }
+
+
+document
+  .querySelector("#submitBtn")
+  .addEventListener("click", theClickEvent => {
+    console.log("111111111111111")
+    submitEntry()
+  }
+)
+
+const submitEntry = () => {
+  const journalDate = document.getElementById("journalDate").value
+  const conceptsCovered = document.getElementById("conceptsCovered").value
+  const journalEntry = document.getElementById("journalEntry").value
+  const moodForTheDay = document.getElementById("moodForTheDay").value
+  console.log(
+    `${journalDate} ${conceptsCovered} ${journalEntry} ${moodForTheDay}`
+  )
+  console.log("222222222")
+}
+// submitEntry()
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
 console.log("Daily Jornal form")
+
+import submitMsg from "./JournalDataProvider.js"
+
+submitMsg()
