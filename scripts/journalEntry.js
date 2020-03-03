f19587e25a584eace1eba37b5f43c7b6ce881a66
Andrew Harper <druharper@gmail.com>
2020-03-03 10:22:05 -0600

    cleaning up css still.. not succeeding in getting object.push done

--- scripts/journalEntry.js
+++ scripts/journalEntry.js
@@ -1,8 +1,5 @@
 console.log("JournalEntry.js loaded")
-/*
- *  Purpose: To render a single journal entry as an
- *           HTML representation of the data
- */
+
 const JournalEntryComponent = entry => {
   return `
     <section id="entry--${entry.id}" class="journalEntry">
